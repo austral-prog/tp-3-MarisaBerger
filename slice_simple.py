63a8bb143d920d1fe488f7452d312d265fce53a0
MarisaBerger <163553459+MarisaBerger@users.noreply.github.com>
2024-04-05 12:22:48 -0300

    Update slice_simple.py

--- slice_simple.py
+++ slice_simple.py
@@ -1,7 +1,10 @@
 def slice_simple():
-    texto = "Awesome"
+    texto = "Awesome".lower()
     # Código a implementar, se debe utilizar la variable 'texto' para resolver el ejercicio.
     # No se debe modificar la definición de la función, ni ingresar otro valor mediante input.
+    print(palabra[0:3])
+    print(palabra[2:5])
+    print(palabra[0:4] + palabra[-3:])
 
 
 # Para verificar este ejercicio ejecutar el comando
