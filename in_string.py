c82104eb568ea631d784de5e6f225ee9db5e4516
MarisaBerger <163553459+MarisaBerger@users.noreply.github.com>
2024-04-05 11:06:28 -0300

    Update in_string.py

--- in_string.py
+++ in_string.py
@@ -1,6 +1,13 @@
 def check_vowels():
     # Código a implementar utilizando input.
-
+    texto = input("Ingresa tu nombre: ").lower()
+    print("Contiene a: " + str("a" in texto))
+    print("Contiene e: " + str("e" in texto))
+    print("Contiene i: " + str("i" in texto))
+    print("Contiene o: " + str("o" in texto))
+    print("Contiene u: " + str("u" in texto))
 
 # Para verificar este ejercicio ejecutar el comando
 # `pytest tp3_in_string_test.py` o `python tp3_in_string_test.py`
+
+
